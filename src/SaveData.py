191ce8d1eb232ac35e74364fc999b82526b4a3af
ave8l <karolave06@gmail.com>
2026-05-29 09:55:58 -0600

    Agregue funciones para apps en uso de RAM y SSD
    Agregue otras 2 funciones, una que revisa las aplicaciones en uso y la cantidad de RAM que ocupa cada una, se puede  modificar a partir de cuantos MB de uso se pueden mostrar las aplicaicones.
    La otra funcion de mostrar las apps por categoria es para la SSD (se tarda mucho, habra ajustes)

--- src/SaveData.py
+++ src/SaveData.py
@@ -1,4 +1,5 @@
 import psutil
+import os
 
 def obtener_ram_global():
     mem =psutil.virtual_memory()
@@ -34,5 +35,83 @@ def obtener_ssd(ruta = 'C:\\'):
     except FileNotFoundError:
         return {"error": "La ruta especificada no existe."}
 
+def listar_apps_ram():
+    procesos = []
+
+    for proc in psutil.process_iter(['pid', 'name', 'memory_info', 'memory_percent']):
+        try:
+            info = proc.info
+            uso_mb = round(info['memory_info'].rss / (1024 **2), 2)
+            if uso_mb > 0: #Modificar para mostrar a partir de cuantos MB se desea mostrar
+                procesos.append({
+                    "pid": info['pid'],
+                    "app": info['name'],
+                    "ram_usada_mb": uso_mb,
+                    "porcentaje": round(info['memory_percent'], 2)
+                })
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    procesos_ordenados = sorted(procesos, key=lambda x: x['ram_usada_mb'], reverse = True)
+
+    return procesos_ordenados
+
+
+
+
+# Para poder listar las apps dentro del SSD o disco duro se necesitan permisos de administrador para
+#ciertas aplicaciones, por lo que a menos de que sea muy necesario no se extraera por el momento
+def obtener_tamano_carpeta(ruta_base):
+    tamano_total = 0
+    # Recorremos la carpeta y subcarpetas sumando el tamaño de cada archivo
+    try:
+        for directorio_raiz, carpetas, archivos in os.walk(ruta_base):
+            for archivo in archivos:
+                ruta_archivo = os.path.join(directorio_raiz, archivo)
+                # Omitimos accesos directos y archivos protegidos para evitar errores
+                if not os.path.islink(ruta_archivo) and os.path.exists(ruta_archivo):
+                    tamano_total += os.path.getsize(ruta_archivo)
+    except (PermissionError, FileNotFoundError):
+        pass # Ignoramos carpetas del sistema a las que no tenemos acceso como usuarios normales
+        
+    return tamano_total
+
+def desglosar_almacenamiento_ssd(): #SE TARDA MUCHISIMOOOOO
+    gb_const = 1024 ** 3
+    usuario = os.getlogin() 
+    
+    #Sistema Operativo (Carpeta Windows)
+    sistema_gb = obtener_tamano_carpeta('C:\\Windows') / gb_const
+    
+    #Aplicaciones Instaladas (Archivos de Programa)
+    apps_gb = (obtener_tamano_carpeta('C:\\Program Files') + 
+               obtener_tamano_carpeta('C:\\Program Files (x86)')) / gb_const
+    
+    #Multimedia (Videos, Imágenes, Música del usuario)
+    rutas_multimedia = [
+        f'C:\\Users\\{usuario}\\Videos',
+        f'C:\\Users\\{usuario}\\Pictures',
+        f'C:\\Users\\{usuario}\\Music'
+    ]
+    multimedia_gb = sum(obtener_tamano_carpeta(ruta) for ruta in rutas_multimedia) / gb_const
+    
+    return {
+        "sistema_gb": round(sistema_gb, 2),
+        "aplicaciones_gb": round(apps_gb, 2),
+        "multimedia_gb": round(multimedia_gb, 2)
+    }
+
+#desglose = desglosar_almacenamiento_ssd()
+#Se tarda mucho en buscar las aplicaciones, se buscara una mejor forma o se descartará
+
+#print("--- DESGLOSE DEL DISCO ---")
+#print(f"Sistema: {desglose['sistema_gb']} GB")
+#print(f"Apps: {desglose['aplicaciones_gb']} GB")
+#print(f"Multimedia: {desglose['multimedia_gb']} GB")
+
+
+apps_ram = listar_apps_ram()
+    
+
 
 print(obtener_ram_global())
+print(obtener_ssd())
